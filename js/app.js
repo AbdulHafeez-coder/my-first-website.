6a573d27f7ceb2f89b0387016fb04cb5a3d17cbc
AbdulHafeez-coder <abdulhafeez8278@gmail.com>
2025-06-30 17:01:17 +0500

    abdul hafeez

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
